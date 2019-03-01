883b7cf665f7e3a1eb1e1fd3d9922f7f14bc019b
zhenx804 <zhenx804@newschool.edu>
2019-03-01 11:17:32 -0500

    .
    .

---
+++ projects/class activity/java_practice/index.js
@@ -0,0 +1,42 @@
+var myName="vesper";
+console.log(myName);
+
+var favNumber=6;
+console.log(favNumber);
+
+var favNumberS="six";
+console.log(favNumberS);
+
+var t=true;
+console.log(t);
+
+var name=['v', 'e', 's', 'p', 'e', 'r'];
+console.log(letters);
+
+var dataTypes=[1, t, "one"];
+console.log(dataTypes);
+
+var fullName="Vesper"+"Zheng";
+console.log(fullName);
+
+var hundred=20+80;
+console.log=(hundred);
+
+for var 
+
+
+var firstItem=name[0];
+var lastItem=name[5];
+
+
+
+
+
+
+
+
+
+
+
+
+
