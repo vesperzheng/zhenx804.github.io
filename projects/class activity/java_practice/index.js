2d34e63280f0b82de79023a12dea8ea664f954f5
zhenx804 <zhenx804@newschool.edu>
2019-03-12 09:14:38 -0400

    ..
    ..

--- projects/class activity/java_practice/index.js
+++ projects/class activity/java_practice/index.js
@@ -20,13 +20,82 @@ var fullName="Vesper"+"Zheng";
 console.log(fullName);
 
 var hundred=20+80;
-console.log=(hundred);
+console.log(hundred);
 
-for var 
+console.log('v', 'r');
+
+var word=('supercalifragilisticexpialidocious');
+console.log(word.length);
 
 
-var firstItem=name[0];
-var lastItem=name[5];
+if (student= 16)
+	{
+		console.log('all the students are present');
+}
+else{
+		console.log('not all students are present');
+}
+
+
+for (var i = 0; i < name.length; i++) { 
+
+	console.log( name[i]);
+}
+
+
+
+if ( hour >= 19 && hour <= 22) {
+  console.log("yes, it's between 7pm and 9:40pm");
+} else  {
+  console.log("it's not between 7pm and 9:40pm");
+}
+
+
+var date = todayDate();
+
+if (date == 1){
+	console.log('Ugh');
+} 
+else if (date ==2){
+	console.log('Okay');
+} 
+else if (date ==3){
+	console.log('Mhm');
+} 
+else if (date ==4){
+	console.log('Oh'); 
+} 
+else if (date ==5){
+	console.log('Yesss'); 
+} 
+else if (date ==6){
+	console.log('zzz'); 
+} 
+else if (date ==7){
+	console.log('HW'); 
+}
+
+
+var favfood =['icecream', 'pizza', 'padthai'];
+var favanimal =['fox', 'dog', 'tiger'];
+var favplaces =['Shenzhen', 'Kyoto', 'Universal Studio'];
+var favpokemon =['Bulbasaur', 'Mewtwo', 'Togepi'];
+console.log(favfood, favanimal, favplaces, favpokemon);
+
+var everything =['favfood', 'favanimal', 'favplaces', 'favpokemon'];
+console.log(everything);
+
+for (var i=0; i < everything.length; i++) {
+	console.log(everything[i]);
+}
+
+
+
+
+
+
+
+
 
 
 
